1c0d0136159404bc0503a01742981edb9c4e6e09
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-09-24 07:26:01 -0400

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Jimmy Ghelani on 2023-09-24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
